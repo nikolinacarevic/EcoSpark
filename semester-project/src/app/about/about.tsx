dd5b1b299342ce07012df9d028e3805b6a954fae
nikolinacarevic <ncarev00@fesb.hr>
2024-01-14 16:24:48 +0100

    Added wellcome text on About Us page

--- semester-project/src/app/about/about.tsx
+++ semester-project/src/app/about/about.tsx
@@ -5,9 +5,17 @@ import styles from './about.module.css'
 
 export default function About() {
   return (
+    <>
       <div className={styles.titleContainer}>
           <h1 id={styles.mainTitle}>About Us</h1>
           <img src='/images/aboutUs.png' id={styles.backgroundImage}></img>
       </div>
+
+    <div className={styles.wellcomeContainer}>
+      <h1 id={styles.wellcomeTitle}>Welcome to EcoSpark – Pioneering Tomorrow's Drive</h1>
+      <p id={styles.wellcomeText}>At EcoSpark, we embark on a revolutionary journey towards a sustainable and emission-free future. Nestled at the forefront of innovation, EcoSpark is not merely an electric car company; we are the architects of a cleaner, greener automotive landscape. Our commitment extends beyond manufacturing electric vehicles; we are crafting a tomorrow where transportation is synonymous with environmental responsibility and cutting-edge technology.</p>
+    </div>
+    
+  </>
   );
 }
