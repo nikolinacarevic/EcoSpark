b40981096da9df3b3b2898f740e26864723b6860
nikolinacarevic <ncarev00@fesb.hr>
2024-01-14 16:53:04 +0100

    Added About Us page for desktop

--- semester-project/src/app/about/about.tsx
+++ semester-project/src/app/about/about.tsx
@@ -16,6 +16,20 @@ export default function About() {
       <p id={styles.wellcomeText}>At EcoSpark, we embark on a revolutionary journey towards a sustainable and emission-free future. Nestled at the forefront of innovation, EcoSpark is not merely an electric car company; we are the architects of a cleaner, greener automotive landscape. Our commitment extends beyond manufacturing electric vehicles; we are crafting a tomorrow where transportation is synonymous with environmental responsibility and cutting-edge technology.</p>
     </div>
     
+    <div className={styles.boxContainer}>
+      <div className={styles.hashtagBox}>
+        <h1 className={styles.hashtagTitle}>#Strategy</h1>
+        <p className={styles.hashtagText}>At EcoSpark, strategy is not just a plan; it's a commitment to innovation and adaptability. We are strategically positioned to lead the charge towards a zero-emission world, investing in cutting-edge technologies and sustainable practices. Our commitment to strategic excellence ensures that every EcoSpark vehicle is not just an automobile but a step towards a cleaner and brighter future.</p>
+      </div>
+      <div className={styles.hashtagBox}>
+        <h1 className={styles.hashtagTitle}>#Integrity</h1>
+        <p className={styles.hashtagText}>Integrity is the heartbeat of EcoSpark. We believe in transparency, accountability, and ethical business practices. Our commitment to integrity extends from the design board to the showroom, ensuring that our customers experience not only the thrill of driving an electric vehicle but also the peace of mind that comes with knowing they have made a responsible choice.</p>
+      </div>
+      <div className={styles.hashtagBox}>
+        <h1 className={styles.hashtagTitle}>#Diversity</h1>
+        <p className={styles.hashtagText}>Diversity is our strength at Ecospark. We celebrate the richness of ideas, perspectives, and experiences that a diverse team brings. From our manufacturing facilities to the executive boardroom, we foster an environment that values inclusivity, equality, and the power of different voices coming together to drive innovation.</p>
+      </div>
+    </div>
   </>
   );
 }
